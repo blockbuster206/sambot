f60cbcc8be13262bce43f32e5fb094b0732cb57f
blockbuster-exe <samscott640@gmail.com>
2021-10-10 11:24:08 +0800

    added start scripts

---
+++ start.bat
@@ -0,0 +1,4 @@
+echo getting latest modules
+pip install -r requirements.txt
+cd sambot
+python bot.py
---
+++ start.sh
@@ -0,0 +1,4 @@
+echo getting latest modules
+python3 -m pip install -r requirements.txt
+cd sambot
+python3 bot.py
